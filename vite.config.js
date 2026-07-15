ff16f74c29c8b6a6acc10e9953a5e2a7410e95b4
Jimtsai <hushyou123@gmail.com>
2026-07-15 10:24:14 +0800

    try git action

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/ORDSearchFast_vite/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
